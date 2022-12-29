403132cefdf0860edacfb48ac9bee2aecdd39eb4
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2022-12-29 10:33:27 +0200

    upload
    adding golden balloon
    change css

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,105 @@
-let ballon = document.querySelector(".ballon")as HTMLImageElement
-ballon.addEventListener("click", onClick)
+console.log("js connected");
+//----------------------------------------------variables------------------------------------------------------------------------
+let balloon = document.querySelector("#balloon")as HTMLImageElement
+let goldenBalloon = document.querySelector("#goldenBalloon") as HTMLImageElement
+var yPos = 101;
 let xPosition = Math.round(Math.random()*80)
 let score = document.querySelector(".score")!
+let startBtn = document.querySelector(".startBtn") as HTMLButtonElement
 let ctrScore = 0
 let life = 3
+let lifeLevel =document.querySelector(".lifeLevel") as HTMLParagraphElement
+let audioBack = document.querySelector(".audioBack") as HTMLAudioElement
+let popSound = document.querySelector(".popSound") as HTMLAudioElement
+let errorSound = document.querySelector(".errorSound") as HTMLAudioElement
+let hundredSound = document.querySelector(".hundredSound") as HTMLAudioElement
 
-ballon.style.left = `${xPosition}%`
+//----------------------------------------------events-----------------------------------------------------------------------------------------------------------
 
-// function onClick(){
-//     ballon.style.animation = 'none';
-//     ballon.offsetLeft;
-//     ballon.style.animation = "ballonRiseUp 5s ease-in-out infinite ,ballonRotate 4s ease-in-out infinite";
-//     xPosition = Math.round(Math.random()*80)
-//     ballon.style.left = `${xPosition}%`
+startBtn.addEventListener("click" ,onStart)
+balloon.addEventListener("click", onClick)
+goldenBalloon.addEventListener("click", goldenClicked)
 
+//----------------------------------------------function----------------------------------------------------------------------------------------------------------
+function onStart(){
+    audioBack.play()
+    startBtn.style.display = "none"
+    myMove()
+}
 
-//     ctrScore+=10
-//     score.textContent = `score: ${ctrScore}`
+let id:any
+function myMove(){
+    xPosition = Math.round(Math.random()*80)
+    balloon.style.left = `${xPosition}%`
+    yPos = 101 //ותוודאי שהשורות האלה קיימות לך טוב
+    balloon.style.top = `${yPos}vh` //וגם זו 
+    id = null
+    clearInterval(id);
+    id = setInterval(frame, 180);
+    function frame() {
+        if (yPos == -35) {
+        clearInterval(id);
+        balloonOver()
+        } else {
+        yPos--; 
+        balloon.style.top = yPos + 'vh'; 
+        }
+    }
+}
 
-// }
+function balloonOver(){
+    life--
+    lifeLevel.textContent = `${life}`
+    errorSound.play()
+    if(life > 0)
+        myMove()
+    else{
+        balloon.remove()
+        audioBack.pause()
+        if (confirm(`GAME-OVER\n you earn ${ctrScore} score`)){
+            location.reload()
+        }
+        startBtn.style.display = "block"
+        life = 4
+        ctrScore= 0
+    }
+}
 
-let
+function onClick(){
+    myMove()
+    ctrScore+=10
+    score.textContent = `score: ${ctrScore}`
+    popSound.play()
+    if(ctrScore > 10 && ctrScore%100 === 0){
+        hundredSound.play()
+        goldenRiseUp()
+    }
+}
+
+//----------------------------------------------golden functions--------------------------------------------------------------------------------------------
+
+function goldenRiseUp(){
+    xPosition = Math.round(Math.random()*80)
+    goldenBalloon.style.left = `${xPosition}%`
+    yPos = 101
+    goldenBalloon.style.top = `${yPos}vh`
+    id = null
+    clearInterval(id);
+    id = setInterval(frame, 100);
+    function frame() {
+        if (yPos == -30) {
+        clearInterval(id);
+        } else {
+        yPos--; 
+        goldenBalloon.style.top = yPos + 'vh'; 
+        }
+    }
+}
+
+function goldenClicked(){
+    yPos = 101
+    goldenBalloon.style.top = `${yPos}vh`
+    ctrScore+=50
+    score.textContent = `score: ${ctrScore}`
+    hundredSound.play()
+}
